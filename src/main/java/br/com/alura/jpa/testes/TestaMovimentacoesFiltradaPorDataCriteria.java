c05e22947997fe2c1ca2a5aed05e3ea89a2defef
odair-pedroso <odairpedroso90@gmail.com>
2020-09-29 07:47:42 -0300

    criteria api

--- src/main/java/br/com/alura/jpa/testes/TestaMovimentacoesFiltradaPorDataCriteria.java
+++ src/main/java/br/com/alura/jpa/testes/TestaMovimentacoesFiltradaPorDataCriteria.java
@@ -1,9 +1,12 @@
 package br.com.alura.jpa.testes;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.jpa.modelo.Movimentacao;
 import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
 
 public class TestaMovimentacoesFiltradaPorDataCriteria {
@@ -14,7 +17,15 @@ public class TestaMovimentacoesFiltradaPorDataCriteria {
 		EntityManager em = emf.createEntityManager();
 
 		MovimentacaoDao movimentacaoDao = new MovimentacaoDao(em);
-		movimentacaoDao.getMovimentacoesFiltradasPorData(dia, mes, ano);
+		List<Movimentacao> movimentacoesFiltradasPorData = movimentacaoDao.getMovimentacoesFiltradasPorData(null, null,
+				2017);
+
+		for (Movimentacao movimentacao : movimentacoesFiltradasPorData) {
+
+			System.out.println("Descrição -> " + movimentacao.getDescricao());
+			System.out.println("Valor -> " + movimentacao.getValor());
+			System.out.println("--------------------------------------------------------");
+		}
 
 	}
 
